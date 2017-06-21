b1446e536119ee1dfd27b4d254b9fbcaf227e2f9
WangYihang <wangyihanger@gmail.com>
2017-06-21 14:38:42 +0800

    pwn350-leak-advanced-x86-64

--- pwn/pwn350-leak-advanced-x86-64/challenge.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-CHALLENGE_FILE_NAME='leak'
-
-su sniper -c "socat tcp-l:65535,reuseaddr,fork exec:/home/sniper/${CHALLENGE_FILE_NAME} &"
---
+++ pwn/pwn350-leak-advanced-x86-64/run.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+# define
+USER='sniper'
+FILE_PATH="/home/${USER}"
+BIN='leak'
+FLAG=''
+
+# set pri
+chown root ${FILE_PATH}
+chgrp root ${FILE_PATH}
+chmod 555 ${FILE_PATH}
+chmod -R 444 ${FILE_PATH}/*
+chmod +x ${FILE_PATH}/${BIN}
+
+# start socat server
+su ${USER} -c "socat tcp-l:65535,reuseaddr,fork exec:${FILE_PATH}/${BIN} &"
+
+# set flag
+echo "SniperOJ{${FLAG}}" > ${FILE_PATH}/flag
+
+# start /bin/sh
+/bin/bash
