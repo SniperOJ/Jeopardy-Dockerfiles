38ca4d9802659b2d79bd25c5b92d4fd255c2e2b4
WangYihang <wangyihanger@gmail.com>
2018-05-31 19:34:28 +0800

    [+] Add challenge r-cursive-revenge

---
+++ web/r-cursive-revenge/source/index.php
@@ -0,0 +1,17 @@
+<?php
+// No more headers, what do you say? >_o
+$token = sha1($_SERVER['REMOTE_ADDR']);
+$dir = './sandbox/'.$token.'/';
+is_dir($dir) ?: mkdir($dir);
+is_file($dir.'index.php') ?: file_put_contents($dir.'index.php', str_replace('#SHA1#', $token, file_get_contents('./template')));
+switch($_GET['action'] ?: ''){
+	case 'go':
+		header('Location: '.$dir);
+		break;
+	case 'reset':
+		system('rm -rf '.$dir);
+		break;
+	default:
+		show_source(__FILE__);
+}
+?>
