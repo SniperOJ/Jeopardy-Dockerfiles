b40a3d22b26fa186783d309357afa70e1ff39bac
Wang Yihang <wangyihanger@gmail.com>
2018-12-03 11:55:44 +0800

    [+] Baby injection

---
+++ web/baby-injection/source/index.php
@@ -0,0 +1,31 @@
+<?php
+ini_set("display_errors", "On");
+error_reporting(E_ALL | E_STRICT);
+$dbhost = "mysql";
+$dbuser = "root";
+$dbpass = "root";
+$db = "ctf";
+$conn = mysqli_connect($dbhost,$dbuser,$dbpass,$db);
+mysqli_set_charset($conn,"utf8");
+
+function   filter($str){
+        $black_list = array("union", "select", "or");
+        foreach ($black_list as $key) {
+            $str = str_replace($key, "", $str);
+        }
+        return $str;
+}
+
+$username = isset($_GET['username'])?filter($_GET['username']):die("Please input username!");
+
+$sql = "select * from `users` where  username ='$username'";
+
+$res = $conn->query($sql);
+
+if($res && $res->num_rows-0){
+        $row = $res -> fetch_assoc();
+        var_dump($row);
+} else {
+    die("no such user");
+}
+?>
